0ba715fc4a6b242fa90ff734cf4e903774acebf6
parasgadal67-oct <gadalparas67@gmail.com>
2026-09-09 18:40:09 +0530

    O.O.P(Object Oriented Programming)
    Object Oriented Programming
    created the person class

---
+++ Day 31/main.py
@@ -0,0 +1,10 @@
+from person import Person
+
+person1 = Person("Paras", 24, "India", "Dehradun", 8394833407)
+person2 = Person("Subhash", 30, "Japan", "Tokyo", 9875903857)
+person3 = Person("Ayush", 32, "India", "Dehradun", 7896543280)
+print(person2.name) 
+print(person2.age) 
+print(person2.country) 
+print(person2.city)
+print(person2.contact_no)  
---
+++ Day 31/person.py
@@ -0,0 +1,9 @@
+# PERSON CLASS FROOM O.O.P
+class Person:
+ def __init__(self, name, age, country, city, contact_no):
+    self.name = name
+    self.age = age
+    self.country = country
+    self.city = city
+    self.contact_no = contact_no
+    
